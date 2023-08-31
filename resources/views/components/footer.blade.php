ff10b5c92346164cf2fb9ae1b0338d96f8a9aa92
Ikram Maulana <ikram075@ummi.ac.id>
2023-08-31 15:45:44 +0700

    Feat: Add Footer Component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,176 @@
+<footer class="bg-zinc-100 border-t relative overflow-hidden">
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 relative z-10">
+        <div class="grid md:grid-cols-4 lg:grid-cols-5 md:gap-6">
+            <div class="mb-6 md:mb-0">
+                <a href="https://flowbite.com/" class="flex items-center">
+                    <img src={{ asset('monitorindonesialogo.png') }} class="w-56 h-[67px]" alt="FlowBite Logo" />
+                </a>
+                <h2 class="mt-2 mb-1 text-sm font-semibold text-zinc-900 uppercase dark:text-white">Download
+                    Sekarang</h2>
+                <div class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 md:items-center">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src={{ asset('googlestore.png') }} class="w-36 md:w-24" alt="Google Store" />
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src={{ asset('applestore.png') }} class="w-36 md:w-24" alt="Apple Store" />
+                    </a>
+                </div>
+            </div>
+            <div class="mb-6 md:mb-0 grid sm:grid-cols-2 gap-8 sm:gap-6 md:col-span-3 lg:col-span-3">
+                <div>
+                    <h2 class="mb-6 text-sm font-semibold text-zinc-900 uppercase dark:text-white">Telusuri</h2>
+                    <ul class="text-zinc-500 dark:text-zinc-400 font-medium grid grid-rows-4 grid-flow-col gap-x-6">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Ekonomi</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Politik</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Hukum</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Pendidikan</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Hobi</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Olahraga</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Nasional</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Global</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Hiburan</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Technology</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Otomotif</a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Gaya Hidup</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="mb-6 text-sm font-semibold text-zinc-900 uppercase dark:text-white">Informasi</h2>
+                    <ul class="text-zinc-500 dark:text-zinc-400 font-medium grid grid-rows-4 grid-flow-col gap-x-6">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Redaksi
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Tentang Kami
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Kode Etik
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                SOP Wartawan
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Disclaimer
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Dewan Pers
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Pedoman
+                            </a>
+                        </li>
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">
+                                <span class="mr-2">•</span>
+                                Pemberitaan
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex flex-col md:col-span-4 lg:col-auto">
+                <form>
+                    <div class="flex">
+                        <label for="subscription"
+                            class="mb-2 text-sm font-medium text-zinc-900 sr-only dark:text-white">Your Email</label>
+                        <div class="relative w-full">
+                            <input type="search" id=subscription"
+                                class="block p-2.5 w-full z-20 text-sm text-zinc-900 bg-zinc-50 rounded-lg border border-zinc-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-zinc-700 dark:border-l-zinc-700  dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:border-blue-500"
+                                placeholder="Email Address" required>
+                            <button type="submit"
+                                class="absolute top-0 right-0 h-full p-2.5 text-sm font-medium text-white bg-red-700 rounded-r-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <div class="flex flex-col mt-4 md:flex-row space-y-2 md:space-y-0 md:space-x-2 md:items-center">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src={{ asset('Logo-MI-Podcast.png') }} class="w-36 md:w-28" alt="Google Store" />
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src={{ asset('Logo-MI-Channel.png') }} class="w-36 md:w-28" alt="Google Store" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr class="my-6 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-8" />
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <span class="text-sm text-zinc-500 dark:text-zinc-300 sm:text-center">All Rights Reserved by <a
+                    href="https://monitorindonesia.com" target="_blank"
+                    rel="noopener noreferrer">monitorindonesia.com</a> © 2023
+            </span>
+            <div class="flex mt-4 space-x-2 sm:justify-center md:mt-0">
+                <a href="#"
+                    class="bg-zinc-900 hover:bg-zinc-800 rounded-md w-6 h-6 transition-colors duration-300">
+                    <x-lucide-instagram class="text-white p-1" target="_blank" rel="noopener noreferrer" />
+                    <span class="sr-only">Instagram page</span>
+                </a>
+                <a href="#"
+                    class="bg-zinc-900 hover:bg-zinc-800 rounded-md w-6 h-6 transition-colors duration-300">
+                    <x-lucide-facebook class="text-white p-1" target="_blank" rel="noopener noreferrer" />
+                    <span class="sr-only">Facebook page</span>
+                </a>
+                <a href="#"
+                    class="bg-zinc-900 hover:bg-zinc-800 rounded-md w-6 h-6 transition-colors duration-300">
+                    <x-lucide-youtube class="text-white p-1" target="_blank" rel="noopener noreferrer" />
+                    <span class="sr-only">Youtube page</span>
+                </a>
+                <a href="#"
+                    class="bg-zinc-900 hover:bg-zinc-800 rounded-md w-6 h-6 transition-colors duration-300">
+                    <x-lucide-twitter class="text-white p-1" target="_blank" rel="noopener noreferrer" />
+                    <span class="sr-only">Twitter page</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="absolute inset-0 flex items-center justify-center md:justify-end pointer-events-none">
+        <img src={{ asset('footer-background.png') }} class="w-full md:w-3/4 lg:w-3/5 xl:w-[44%] opacity-20"
+            alt="FlowBite Logo" />
+    </div>
+</footer>
