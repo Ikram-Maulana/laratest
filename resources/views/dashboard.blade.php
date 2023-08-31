11cbf8b9f628c1ee1dd33f93305e37b9fcda3767
Deri-Kurniawan <deri.netuchi@gmail.com>
2023-08-31 16:23:14 +0700

    Feat: Add Dashboard Boiler Plates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,81 @@
+@push('script')
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+@endpush
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h1 class="text-3xl font-bold">Dashboard</h1>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="grid grid-cols-6 gap-4 mb-4 lg:grid-cols-9">
+        <div
+            class="col-span-3 px-6 pt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex flex-row items-center justify-start gap-4">
+                <div class="p-6 text-white bg-red-900 rounded-lg">
+                    <x-lucide-newspaper class="w-12 h-12" />
+                </div>
+                <div>
+                    <dt class="mb-2 text-5xl font-extrabold">32</dt>
+                    <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Live News</dd>
                 </div>
             </div>
+            <hr class="my-3 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-3" />
+            <div class="flex flex-row items-center gap-4 pb-2">
+                <dt class="text-base font-semibold">0</dt>
+                <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Unpublished News</dd>
+            </div>
         </div>
+        <div
+            class="col-span-3 px-6 pt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex flex-row items-center justify-start gap-4">
+                <div class="p-6 text-white bg-red-900 rounded-lg">
+                    <x-lucide-newspaper class="w-12 h-12" />
+                </div>
+                <div>
+                    <dt class="mb-2 text-5xl font-extrabold">32</dt>
+                    <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Live News</dd>
+                </div>
+            </div>
+            <hr class="my-3 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-3" />
+            <div class="flex flex-row items-center gap-4 pb-2">
+                <dt class="text-base font-semibold">0</dt>
+                <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Unpublished News</dd>
+            </div>
+        </div>
+        <div
+            class="col-span-3 px-6 pt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex flex-row items-center justify-start gap-4">
+                <div class="p-6 text-white bg-red-900 rounded-lg">
+                    <x-lucide-newspaper class="w-12 h-12" />
+                </div>
+                <div>
+                    <dt class="mb-2 text-5xl font-extrabold">32</dt>
+                    <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Live News</dd>
+                </div>
+            </div>
+            <hr class="my-3 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-3" />
+            <div class="flex flex-row items-center gap-4 pb-2">
+                <dt class="text-base font-semibold">0</dt>
+                <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Unpublished News</dd>
+            </div>
+        </div>
+        <div
+            class="col-span-3 px-6 pt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex flex-row items-center justify-start gap-4">
+                <div class="p-6 text-white bg-red-900 rounded-lg">
+                    <x-lucide-newspaper class="w-12 h-12" />
+                </div>
+                <div>
+                    <dt class="mb-2 text-5xl font-extrabold">32</dt>
+                    <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Live News</dd>
+                </div>
+            </div>
+            <hr class="my-3 border-zinc-200 sm:mx-auto dark:border-zinc-700 lg:my-3" />
+            <div class="flex flex-row items-center gap-4 pb-2">
+                <dt class="text-base font-semibold">0</dt>
+                <dd class="font-semibold text-gray-500 dark:text-gray-400">Total Unpublished News</dd>
+            </div>
+        </div>
+    </div>
     </div>
 </x-app-layout>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,75 @@
+<aside id="logo-sidebar"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-medium">
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <x-lucide-layout-dashboard
+                        class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                    <span class="ml-3">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <button type="button"
+                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <x-lucide-newspaper
+                        class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap">News</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 4 4 4-4" />
+                    </svg>
+                </button>
+                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">News
+                            Category</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">News
+                            Sub Category</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">News
+                            Speciality</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">All
+                            News</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">News
+                            Bulk Add</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Archieve
+                            News</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">News
+                            Serial</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="{{ route('dashboard') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <x-lucide-users
+                        class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                    <span class="ml-3">Manage User</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
